9a0273ec07dea5165b381153b5e82a731ae32adf
Cinotti02 <francesco.cinotti@edu.unifi.it>
2025-03-26 18:05:40 +0100

    Add disclaimer message for program operation in main function

--- main.cpp
+++ main.cpp
@@ -2,6 +2,15 @@
 #include "Utility.h"
 
 int main() {
+    cout << endl;
+    cout << "-----------------------------------------------------------------------------------------------------------------" << endl;
+    cout << "|    " SPACE RED "              Disclamer !!!" RESET SPACE  "                  |"<< endl;
+    cout << "|      for the correct operation of the program, please run the following command in the powershell terminal    |" << endl;
+    cout << "|             Command:" GREEN "    reg add \"HKCU\\Console\" /v VirtualTerminalLevel /t REG_DWORD /d 1 /f" RESET "                   |" << endl;
+    cout << "|                    this command allows the program to display the colored text correctly                      |" << endl;
+    cout << "-----------------------------------------------------------------------------------------------------------------" << endl;
+    cout << endl;
+
     Interface inter;
     loadTaskFromFile(inter.getFilePath(), inter.getTaskList(), inter.getCompletedTaskList());
     bool running = true;
