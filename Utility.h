305f697aeb2e1feede1dd948882e595438296976
Cinotti02 <francesco.cinotti@edu.unifi.it>
2025-03-28 09:41:19 +0100

    Add comments to Utility.h functions for improved clarity

--- Utility.h
+++ Utility.h
@@ -12,17 +12,17 @@
 
 using namespace std;
 
-string readFile(const string &nameFile);
+string readFile(const string &nameFile);  //Legge il contenuto di un file e lo restituisce come stringa
 
-void writeFile(const string &nameFile, const string &content);
+void writeFile(const string &nameFile, const string &content);  //Scrive il contenuto di una stringa in un file
 
-void loadTaskOnJFile(const string &nameFile, list<Task> &tasks, list<Task> &completed);
+void loadTaskOnJFile(const string &nameFile, list<Task> &tasks, list<Task> &completed);  //Carica le attività da una lista di attività e le memorizza in una un file JSON
 
-void loadTaskFromFile(const string &nameFile, list<Task> &tasks, list<Task> &completed);
+void loadTaskFromFile(const string &nameFile, list<Task> &tasks, list<Task> &completed);  //Carica le attività da un file JSON e le memorizza in una lista di attività
 
-void controlCin(int &choice);
+void controlCin(int &choice);  //Controlla l'input dell'utente e gestisce gli errori
 
-void controlCinData(int &d, int &m, int &y);
+void controlCinData(int &d, int &m, int &y);  //Controlla l'input della data dell'utente e gestisce gli errori
 
 static void cinFile();
 
