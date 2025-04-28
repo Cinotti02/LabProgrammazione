ee3101bfa1522ae44e746efbdc3dd658a25e3e57
Cinotti02 <francesco.cinotti@edu.unifi.it>
2025-04-28 15:40:59 +0200

    Refactor main function to replace Interface with ToDoList for task management and streamline menu handling

--- main.cpp
+++ main.cpp
@@ -4,29 +4,28 @@
 int main() {
     cout << endl;
     cout << "-----------------------------------------------------------------------------------------------------------------" << endl;
-    cout << "|    " SPACE RED "          !!! Disclaimer !!!" RESET SPACE  "                  |"<< endl;
+    cout << "|    " SPACE RED "          !!! Disclaimer !!!" RESET SPACE  "                 |"<< endl;
     cout << "|      for the correct operation of the program, please run the following command in the powershell terminal    |" << endl;
     cout << "|             Command:" GREEN "    reg add \"HKCU\\Console\" /v VirtualTerminalLevel /t REG_DWORD /d 1 /f" RESET "                   |" << endl;
     cout << "|                    this command allows the program to display the colored text correctly                      |" << endl;
     cout << "-----------------------------------------------------------------------------------------------------------------" << endl;
     cout << endl;
 
-    Interface inter;
+    ToDoList list;
     try {
-        loadTaskFromFile(inter.getFilePath(), inter.getTaskList(), inter.getCompletedTaskList());
+        loadTaskFromFile(list);
     } catch (std::runtime_error& e) {
         cerr << "Fatal error \"" << e.what() << "\" the program can't keep running "<< endl;
         return 1;
     }
 
     bool running = true;
-    int choice = 0;
+    int c = 0;
 
-    inter.todayTasks();
     while (running) {
         try {
-            choice = Interface::menu();
-            running = inter.choice(choice);
+            c = menu();
+            running = choice(c, list);
 
         } catch (std::runtime_error &e) {
             cerr << "Fatal error \"" << e.what() << "\" the program can't keep running "<< endl;
